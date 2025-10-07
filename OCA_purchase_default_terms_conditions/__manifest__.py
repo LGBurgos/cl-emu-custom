89edb5a70bb310f730b819ea0d1fdfda5ba1ce6f
Francisco Germani <134541679+FranGermani@users.noreply.github.com>
2025-10-07 13:20:07 -0300

    [UPD] modulo de la OCA

--- OCA_purchase_default_terms_conditions/__manifest__.py
+++ OCA_purchase_default_terms_conditions/__manifest__.py
@@ -1,21 +1,25 @@
 # Copyright 2020 - TODAY, Escodoo
-# Copyright (C) 2022 Open Source Integrators
+# Copyright (C) 2022 OSI
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Purchase Default Terms Conditions",
-    "summary": """
-        This module allows purchase default terms & conditions""",
+    "summary": "Purchase default terms & conditions",
     "version": "18.0.1.0.0",
     "license": "AGPL-3",
-    "author": "Escodoo,Odoo Community Association (OCA)",
+    "author": "Escodoo, OCA",
     "website": "https://github.com/OCA/purchase-workflow",
     "images": ["static/description/banner.png"],
-    "depends": [
-        "purchase",
-    ],
+    "depends": ["purchase"],
     "data": [
         "views/res_config_settings.xml",
         "views/res_partner_views.xml",
+        "views/purchase_order_terms_view.xml",
+        "reports/purchase_terms_report_action.xml",
     ],
+    "qweb": [
+        "reports/purchase_terms_report_template.xml",
+    ],
+    "installable": True,
+    "application": False,
 }
