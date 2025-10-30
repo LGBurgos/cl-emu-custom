50102c065be1f97f05816f8cbafa7d6393f27289
mmarziali <matiasmarziali96@gmail.com>
2025-10-30 18:57:13 -0300

    [FIX] emu_account_bank_statement_notes

--- emu_account_bank_statement_notes/__manifest__.py
+++ emu_account_bank_statement_notes/__manifest__.py
@@ -14,8 +14,13 @@
     'data': [
         'security/ir.model.access.csv',
         'views/bank_statement_note_views.xml',
-        # 'views/account_bank_statement_views_inherited.xml',
+        'views/account_bank_statement_views_inherited.xml',
     ],
+    'assets': {
+            'web.assets_backend': [
+                'emu_account_bank_statement_notes/static/src/xml/bank_rec_record_notebook.xml',
+            ],
+        },
     'installable': True,
     'application': False,
     'sequence': 1,
