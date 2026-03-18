40598726e4844667ef79cba2dbfabd83710ec9da
mmarziali <matiasmarziali96@gmail.com>
2026-03-18 16:36:41 -0300

    [FIX] hito_emu_billing_selecting_delivery_notes: boton inteligente

--- hito_emu_billing_selecting_delivery_notes/__manifest__.py
+++ hito_emu_billing_selecting_delivery_notes/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "hito emu billing selecting delivery notes",
     "version": "18.0",
-    "depends": ['sale_management','accountant','stock_picking_invoice_link'],
+    "depends": ['sale_management','accountant','stock_picking_invoice_link', 'stock_voucher'],
     "category": "",
     "summary": "billing selecting delivery notes",
     "data": [
