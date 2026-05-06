6074b96f80f7de47adf2bd2d8f035f9c3275b1f1
NelsonTontarelli <nelartz01@gmail.com>
2026-05-06 09:47:37 -0300

    [ADD] Modulo fix para emu

---
+++ fix_l10n_ar_tax_settlement_amounts/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Fix AR Tax Settlement Amounts Compatibility",
+    "version": "18.0.1.0.0",
+    "category": "Accounting",
+    "author": "Custom",
+    "license": "LGPL-3",
+    "depends": [
+        "l10n_ar_account_tax_settlement",
+    ],
+    "data": [],
+    "installable": True,
+    "application": False,
+}
